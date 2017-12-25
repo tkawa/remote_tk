f2642afb1754bb8c761f53927564eaa9aee81150
Toru KAWAMURA <tkawa@4bit.net>
2017-12-25 17:54:33 +0900

    Initial implement that works

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem 'broadlink_rm', github: 'sleighter/broadlink_rm'
+gem 'tk'
---
+++ app.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+require 'broadlink_rm'
+require 'tk'
+
+CODE_POWER = [38, 0, 80, 0, 0, 1, 41, 146, 20, 17, 20, 17, 20, 54, 20, 17, 20, 17, 20, 17,
+ 20, 17, 20, 54, 20, 17, 20, 17, 20, 17, 20, 17, 20, 17, 20, 54, 20, 54, 20, 53,
+ 20, 17, 20, 17, 20, 17, 20, 17, 20, 17, 20, 54, 20, 17, 21, 16, 21, 53, 20, 54,
+ 20, 54, 20, 53, 20, 54, 20, 17, 20, 54, 20, 54, 21, 0, 5, 141].freeze
+CODE_SOURCE = [38, 0, 88, 0, 0, 1, 40, 147, 19, 18, 19, 18, 18, 56, 19, 18, 19, 18, 18, 19,
+ 19, 18, 19, 55, 19, 18, 18, 19, 19, 18, 19, 18, 19, 18, 19, 55, 19, 54, 19, 55,
+ 19, 18, 19, 18, 18, 19, 19, 18, 18, 19, 19, 18, 19, 55, 19, 18, 19, 55, 19, 55,
+ 19, 54, 19, 55, 19, 55, 19, 55, 19, 18, 18, 56, 19, 0, 5, 143].freeze
+CODE_VOLUME_UP = [38, 0, 88, 0, 0, 1, 41, 146, 20, 17, 20, 17, 20, 54, 20, 17, 21, 16, 21, 16,
+ 21, 16, 20, 54, 20, 17, 20, 17, 20, 17, 20, 17, 20, 17, 20, 53, 21, 53, 21, 53,
+ 20, 17, 20, 17, 20, 17, 20, 17, 21, 16, 21, 53, 21, 53, 21, 16, 20, 54, 20, 53,
+ 20, 54, 21, 53, 21, 53, 20, 17, 20, 17, 20, 53, 21, 0, 5, 142].freeze
+CODE_VOLUME_DOWN = [38, 0, 88, 0, 0, 1, 41, 146, 21, 16, 20, 17, 20, 54, 20, 17, 20, 17, 20, 17,
+ 20, 17, 20, 54, 20, 17, 20, 17, 20, 17, 20, 17, 20, 17, 20, 53, 21, 53, 21, 53,
+ 20, 54, 20, 17, 20, 17, 20, 17, 20, 17, 20, 54, 20, 54, 20, 16, 21, 16, 21, 53,
+ 20, 54, 20, 54, 20, 54, 20, 17, 20, 17, 20, 53, 21, 0, 5, 142].freeze
+
+device = BroadlinkRM::Device.discover
+device.auth
+
+TkRoot.new do
+  title 'RM'
+end
+TkButton.new do
+  text '電源'
+  command do
+    device.send_data(CODE_POWER)
+  end
+  pack 'fill' => 'both', 'expand' => 'yes'
+end
+TkButton.new do
+  text '入力切換'
+  command do
+    device.send_data(CODE_SOURCE)
+  end
+  pack 'fill' => 'both', 'expand' => 'yes'
+end
+TkButton.new do
+  text '音量＋'
+  command do
+    device.send_data(CODE_VOLUME_UP)
+  end
+  pack 'fill' => 'both', 'expand' => 'yes'
+end
+TkButton.new do
+  text '音量ー'
+  command do
+    device.send_data(CODE_VOLUME_DOWN)
+  end
+  pack 'fill' => 'both', 'expand' => 'yes'
+end
+
+Tk.mainloop
